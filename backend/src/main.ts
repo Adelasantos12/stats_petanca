161613dc25d06add2fd3f84f7fcaa7dfb3d2a921
Adelasantos12 <adela.santos12@gmail.com>
2026-02-22 12:45:51 +0100

    Update CORS settings and app listen configuration
    Changed CORS credentials setting to false and updated listen method to specify host.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,19 +4,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+    credentials: false,
     optionsSuccessStatus: 204,
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT || 4000);
+
+  const port = Number(process.env.PORT) || 4000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap().catch(err => console.error(err));
